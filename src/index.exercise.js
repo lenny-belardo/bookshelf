3550c58b36f95742ccb85944869f971d013d86b4
x-factor <raoul@appfactor.io>
2024-07-21 15:15:12 +0300

    exercises/01-bootstrap: opened a modal when the login or register buttons is pressed

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,34 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
 import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
-function onButtonClick(buttonName) {
-    alert(`${buttonName} button was clicked!`)
+const BUTTON_KEY = {
+  LOGIN: 'login',
+  REGISTER: 'register'
 }
 
 function App() {
-    return (
+  const [openModal, setOpenModal] = React.useState('none')
+
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+
+      <div>
+        <button onClick={() => setOpenModal('login')}>Login</button>
+      </div>
+
+      <div>
+        <button onClick={() => setOpenModal('register')}>Register</button>
+      </div>
+
+      <Dialog aria-label="login" isOpen={openModal === BUTTON_KEY.LOGIN}>
         <div>
-            <Logo />
-
-            <h2>Bookshelf</h2>
-
-            <div>
-                <button onClick={() => onButtonClick('Login')}>
-                    Login
-                </button>
-            </div>
-
-            <div>
-                <button onClick={() => onButtonClick('Register')}>
-                    Register
-                </button>
-            </div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
-    )
+
+        <h3>Login</h3>
+      </Dialog>
+
+      <Dialog aria-label="register" isOpen={openModal === BUTTON_KEY.REGISTER}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+
+        <h3>Register</h3>
+      </Dialog>
+    </div>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const root = createRoot(document.getElementById('root'))
 root.render(<App />)
-
+export {root}
