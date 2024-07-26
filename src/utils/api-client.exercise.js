30b39af53f940c2f61021ecdaa68a44d0ee6e9fb
x-factor <raoul@appfactor.io>
2024-07-26 23:19:41 +0300

    exercises/03-data-fetching: used the reusable useAsync hook to make and manage the book API call

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -8,13 +8,11 @@ function client(endpoint, customConfig = {}) {
     .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
     .then(async response => {
       const data = await response.json()
-
       if (response.ok) {
         return data
       } else {
         return Promise.reject(data)
       }
-
     })
 }
 
