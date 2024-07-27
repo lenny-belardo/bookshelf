883dd2f431860bb315a91d16c2f1ab86ede7d5c0
x-factor <raoul@appfactor.io>
2024-07-27 17:44:18 +0300

    exercises/04-authentication: fixed the problem when a full-page reload happens and the login page shows up

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,39 +5,39 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client.exercise'
+
+async function getUser() {
+  let data = null
+  const token = await auth.getToken()
+
+  if (token) {
+    data = await client('me', {token})
+  }
+
+  return data
+}
 
 function App() {
   const [user, setUser] = React.useState(null)
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
-
   const logout = () => {
     auth.logout()
-
     setUser(null)
   }
 
-  if (user) {
-    return (
-      <AuthenticatedApp
-        user={user}
-        logout={logout}
-      />
-    )
-  }
-
-  return (
-    <UnauthenticatedApp
-      login={login}
-      register={register}
-    />
+  React.useEffect(() => {
+    getUser().then(data => {
+      setUser(data.user)
+    })
+  }, [])
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
   )
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
