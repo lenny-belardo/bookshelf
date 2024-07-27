f86069923c19f91724c7e0d62468a870dd9f1193
x-factor <raoul@appfactor.io>
2024-07-27 20:54:51 +0300

    exercises/04-authentication: prepared the client to handle POST requests

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,27 +1,28 @@
 import * as auth from 'auth-provider'
-
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, data, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
     headers: {
+      'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
     },
+    body: data ? JSON.stringify(data) : undefined,
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
       await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
-
-      return Promise.reject({message: 'Please re-authenticate'})
+      return Promise.reject({message: 'Please re-authenticate.'})
     }
-
     const data = await response.json()
     if (response.ok) {
       return data
