e4a1ee8121b6508d7dd6f065a382d08e314562ee
x-factor <raoul@appfactor.io>
2024-08-03 11:10:56 +0300

    exercises/06-cache-management: implemented a separate hook for removing and creating a list item (useRemoveListItem, useCreatListItem)

--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -1,6 +1,8 @@
 import {queryCache, useMutation, useQuery} from 'react-query'
 import {client} from 'utils/api-client'
 
+const defaultMutationOptions = {onSettled: () => queryCache.invalidateQueries('list-items')}
+
 function useListItems(user) {
     const {data: listItems} = useQuery({
         queryKey: 'list-items',
@@ -24,8 +26,23 @@ function useUpdateListItem(user) {
             data: updates,
             token: user.token,
           }),
-        {onSettled: () => queryCache.invalidateQueries('list-items')},
+        defaultMutationOptions,
       )
 }
 
-export {useListItem, useListItems, useUpdateListItem}
+
+function useRemoveListItem(user) {
+    return useMutation(
+        ({id}) => client(`list-items/${id}`, {method: 'DELETE', token: user.token}),
+        defaultMutationOptions,
+    )
+}
+
+function useCreateListItem(user) {
+    return useMutation(
+        ({bookId}) => client(`list-items`, {data: {bookId}, token: user.token}),
+        defaultMutationOptions,
+    )
+}
+
+export {useListItem, useCreateListItem, useListItems, useRemoveListItem, useUpdateListItem}
