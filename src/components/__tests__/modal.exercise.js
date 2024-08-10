527bd252534a23762d738a41d9018baf85fa388a
x-factor <raoul@appfactor.io>
2024-08-10 15:05:10 +0300

    exercises/12-testing-hooks-and-components: implemented the unit test for the reusable <Modal /> component

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,11 +1,41 @@
-// 🐨 you're gonna need this stuff:
-// import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import React from 'react'
+import {render, screen, within} from '@testing-library/react'
+import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import userEvent from '@testing-library/user-event'
 
-test.todo('can be opened and closed')
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
-// 💰 Remember all userEvent utils are async, so you need to await them.
+test('can be opened and closed', async () => {
+    const content = "Modal contents"
+    const modalTitle = "Test"
+    const modalLabel = "test-label"
+
+    render(
+        <Modal>
+            <ModalOpenButton>
+                <button>Open modal</button>
+            </ModalOpenButton>
+            <ModalContents aria-label={modalLabel} title={modalTitle}>
+                <div>{content}</div>
+            </ModalContents>
+        </Modal>
+    )
+    expect(screen.getByRole('button', {name: /Open modal/})).toBeInTheDocument()
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+
+    await userEvent.click(screen.getByRole('button', {name: /Open modal/}))
+
+    const modal = screen.getByRole('dialog')
+
+    expect(screen.queryByRole('dialog')).toBeInTheDocument()
+    expect(modal).toBeInTheDocument()
+    expect(modal).toHaveAttribute('aria-label', modalLabel)
+    expect(screen.getByLabelText(modalLabel)).toBeInTheDocument()
+
+    const inModal = within(modal)
+
+    expect(inModal.getByRole('heading', {name: modalTitle})).toBeInTheDocument()
+    expect(inModal.getByText(content)).toBeInTheDocument()
+
+    await userEvent.click(screen.getByRole('button', {name: /Close/}))
+
+    expect(screen.queryByLabelText(modalLabel)).not.toBeInTheDocument()
+})
