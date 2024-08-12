1f15926a015804b358cdb10b5fa7450a596e5ee2
x-factor <raoul@appfactor.io>
2024-08-12 09:36:23 +0300

    exercises/13-integration-testing: refactored the previously implemented code as per solutions

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,78 +1,70 @@
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {queryCache} from 'react-query'
-import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
+import {buildUser, buildBook} from 'test/generate'
 import {AppProviders} from 'context'
 import {App} from 'app'
 
+// general cleanup
 afterEach(async () => {
-    queryCache.clear()
-    await auth.logout()
+  queryCache.clear()
+  await auth.logout()
 })
 
 test('renders all the book information', async () => {
-    const user = buildUser()
-    window.localStorage.setItem(auth.localStorageKey, 'SOME_TOKEN')
+  const user = buildUser()
+  window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
 
-    const book = buildBook()
-    window.history.pushState({}, 'Test page', `/book/${book.id}`)
+  const book = buildBook()
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'Test page', route)
 
-    const originalFetch = window.fetch
-    window.fetch = async (url, config) => {
-        console.log("url", url)
-        if (url.endsWith('/bootstrap')) {
-            return {
-                ok: true,
-                json: async () => ({
-                    user: {
-                        ...user,
-                        token: 'SOME_TOKEN'
-                    },
-                    listItems: [],
-                })
-            }
-        } else if (url.endsWith(`/books/${book.id}`)) {
-            return {
-                ok: true,
-                json: async () => ({book})
-            }
-        }
-
-        return originalFetch(url, config)
+  const originalFetch = window.fetch
+  window.fetch = async (url, config) => {
+    if (url.endsWith('/bootstrap')) {
+      return {
+        ok: true,
+        json: async () => ({
+          user: {...user, token: 'SOME_FAKE_TOKEN'},
+          listItems: [],
+        }),
+      }
+    } else if (url.endsWith(`/books/${book.id}`)) {
+      return {ok: true, json: async () => ({book})}
     }
+    return originalFetch(url, config)
+  }
 
-    render(<App />, {wrapper: AppProviders})
-    await waitForElementToBeRemoved(() => [
-        ...screen.queryAllByText(/loading/i),
-        ...screen.queryAllByLabelText(/loading/i)
-    ])
+  render(<App />, {wrapper: AppProviders})
 
-    expect(screen.getByText(book.title)).toBeInTheDocument()
-    expect(screen.getByText(book.author)).toBeInTheDocument()
-    expect(screen.getByText(book.publisher)).toBeInTheDocument()
-    expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
 
-    expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute('src', book.coverImageURL)
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.publisher)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
 
-    expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
-    expect(screen.queryByRole('button', {name: /remove from list/i})).not.toBeInTheDocument()
-    expect(screen.queryByRole('button', {name: /mark as read/i})).not.toBeInTheDocument()
-    expect(screen.queryByRole('button', {name: /mark as unread/i})).not.toBeInTheDocument()
-    expect(screen.queryByRole('textarea', {name: /notes/i})).not.toBeInTheDocument()
-    expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
-
-    expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
-
-    // screen.debug()
-
-    // 🐨 reassign window.fetch to another function and handle the following requests:
-    // - url ends with `/bootstrap`: respond with {user, listItems: []}
-    // - url ends with `/list-items`: respond with {listItems: []}
-    // - url ends with `/books/${book.id}`: respond with {book}
-    // 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
-
-
-    // 🐨 assert the book's info is in the document
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textbox', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
 })
-
