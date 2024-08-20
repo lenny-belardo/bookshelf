a5f9ad680d61d903a6b86b3c0976fef019f34dd2
x-factor <raoul@appfactor.io>
2024-08-20 10:32:00 +0300

    exercises/09-performance: used the <Profiler /> component in the <ListItemList /> component

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -2,6 +2,7 @@
 import {jsx} from '@emotion/core'
 
 import {useListItems} from 'utils/list-items'
+import {Profiler} from 'components/profiler'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
 
@@ -22,13 +23,15 @@ function ListItemList({filterListItems, noListItems, noFilteredListItems}) {
   }
 
   return (
-    <BookListUL>
-      {filteredListItems.map(listItem => (
-        <li key={listItem.id} aria-label={listItem.book.title}>
-          <BookRow book={listItem.book} />
-        </li>
-      ))}
-    </BookListUL>
+    <Profiler id="List Item List" metadata={{listItemCount: filteredListItems.length}}>
+      <BookListUL>
+        {filteredListItems.map(listItem => (
+          <li key={listItem.id} aria-label={listItem.book.title}>
+            <BookRow book={listItem.book} />
+          </li>
+        ))}
+      </BookListUL>
+    </Profiler>
   )
 }
 
