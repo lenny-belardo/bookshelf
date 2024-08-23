84b3434986a9cc47c2beb45016e7fd06c7f8b6f0
x-factor <raoul@appfactor.io>
2024-08-23 11:14:55 +0300

    exercises/07-context: created a separate <AuthContextProvider /> and extracted there big part of the <App /> component

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,6 +1,16 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
 import * as React from 'react'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
+import * as colors from '../styles/colors'
+import {FullPageSpinner} from '../components/lib'
+import {client} from '../utils/api-client'
+import {useAsync} from '../utils/hooks'
 
 const AuthContext = React.createContext()
+
+AuthContext.displayName = 'AuthContext'
 
 function useAuth() {
   const context = React.useContext(AuthContext)
@@ -10,4 +20,73 @@ function useAuth() {
   return context
 }
 
-export {AuthContext, useAuth}
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider({children}) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  console.log("user ", user)
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    queryCache.clear()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    const props = {user, login, register, logout}
+    return (
+      <AuthContext.Provider value={props}>
+        {children}
+      </AuthContext.Provider>
+    )
+  }
+}
+
+export {AuthProvider, useAuth}
