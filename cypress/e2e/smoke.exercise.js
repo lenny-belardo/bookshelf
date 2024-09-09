062cd5990bd7d09e4ab83570f609aeba6463a38f
x-factor <raoul@appfactor.io>
2024-09-09 23:23:59 +0300

    exercises/14-e2e-testing: added to the e2e test the flow of marking a book as read, rate it and check whether it appears among the list of finished books with the previously set rating

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -31,6 +31,25 @@ describe('smoke', () => {
       cy.findByRole('link', {name: /voie of war/i}).click()
     })
 
+    cy.findByRole('main').within(() => {
+      cy.findByRole('textbox', {name: /notes/i}).type('This is an awesome book')
+      cy.findByLabelText(/loading/i).should('exist')
+      cy.findByLabelText(/loading/i).should('not.exist')
+
+      cy.findByRole('button', {name: /mark as read/i}).click()
+
+      cy.findByRole('radio', {name: /5 star/i}).click({force: true})
+    })
+
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
+
+    cy.findByRole('main').within(() => {
+      cy.findAllByRole('listitem').should('have.length', 1)
+      cy.findByRole('radio', {name: /5 star/i}).should('be.checked')
+      cy.findByRole('link', {name: /voie of war/i}).click()
+    })
     // 🐨 type in the notes textbox
     // The textbox is debounced, so the loading spinner won't show up immediately
     // so to make sure this is working, we need to wait for the spinner to show up
